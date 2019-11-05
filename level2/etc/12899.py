59314af2ebb6bd51ad4a326fca5a003f6e055958
secretdsy <secretdsy@naver.com>
2019-11-05 21:15:30 +0900

    124 나라의 숫자

---
+++ level2/etc/12899.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = []
+    if n == 1:
+        return '1'
+    while(n != 0):
+        b = (n-1)%3
+        n = (n-1)//3
+        answer.append(b)
+    answer.reverse()
+    answer = list(map(str, answer))
+    answer = ''.join(answer)
+    answer = answer.replace('2','4')
+    answer = answer.replace('1','2')
+    answer = answer.replace('0','1')
+    return answer
+
+# N=18
+# print(solution(N))
