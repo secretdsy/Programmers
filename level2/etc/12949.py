4250e58d90155b207220ce90460feba68be0f38e
secretdsy <secretdsy@naver.com>
2019-11-06 12:40:57 +0900

    matrix mul

--- level2/etc/12949.py
+++ level2/etc/12949.py
@@ -1,20 +1,17 @@
 def solution(arr1, arr2):
-    #tmp = [0 for _ in range(len(arr2[0]))]
-    #answer = [tmp for _ in range(len(arr1))]
-    answer = [([0 for _ in range(len(arr2[0]))]) for _ in range(len(arr1))]
-    print(answer)
-    for k in range(len(arr1[0])): #2
-        for i in range(len(arr1)): #3
-            for j in range(len(arr2[0])):#3
+    # 결과 리스트를 0으로 초기화
+    answer = [[0] * len(arr2[0]) for _ in range(len(arr1))]
+    # matrix_size = (i,k) * (k,j) = (i,j)
+    for k in range(len(arr1[0])):
+        for i in range(len(arr1)):
+            for j in range(len(arr2[0])):
                 answer[i][j] += (arr1[i][k] * arr2[k][j])
-    
     
     return answer
 
 
-
-arr1=[[1, 4], [3, 2], [4, 1]]
-arr2=[[1, 2], [3, 4]]
-#arr1=[[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-#arr2=[[5, 4, 3], [2, 4, 1], [3, 1, 1]]
-print(solution(arr1,arr2))
+ar1=[[1, 4], [3, 2], [4, 1]]
+ar2=[[1, 2], [3, 4]]
+#ar1=[[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+#ar2=[[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+print(solution(ar1, ar2))
