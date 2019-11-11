0e4a3e312941e29cdd0dffe77f13eb8a01a7438c
Doyoung Kim <secretdsy@naver.com>
2019-11-12 01:34:47 +0900

    탑

--- level2/stack_queue/42588.py
+++ level2/stack_queue/42588.py
@@ -1,16 +1,12 @@
 def solution(heights):
     answer = []
     for i in range(len(heights)-1, 0, -1):
-
         for j in range(i, 0, -1):
             if heights[i] < heights[j-1]:
                 answer.append(j)
                 break
-
             elif j==1:
                 answer.append(0)
-
     answer.append(0)
     answer.reverse()
-
     return answer
