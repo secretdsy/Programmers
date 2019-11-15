9e01f9f32455599c92381552a34e9a48ce98d320
Doyoung Kim <secretdsy@naver.com>
2019-11-15 19:22:57 +0900

    N개의 최소공배수

--- level2/practice/12953.py
+++ level2/practice/12953.py
@@ -1,4 +1,4 @@
-def gcd(a,b):
+def gcd(a, b):
     r = a%b
     while r > 0:
         a = b
@@ -7,15 +7,14 @@ def gcd(a,b):
     return b
 
 
-def lcm(a,b):
-    return a*b//gcd(a,b)
+def lcm(a, b):
+    return a*b//gcd(a, b)
 
 
 def solution(arr):
     arr.sort()
-    k = lcm(arr[0],arr[1])
+    k = lcm(arr[0], arr[1])
     i = 2
-    
     while (i < len(arr)):
         k = lcm(k, arr[i])
         i+=1
